832cb1aa56683cc794a91c9373cbf526c9efb4db
ciglenn6 <ciglenn6@gmail.com>
2017-02-02 14:37:48 -0500

    homework with animation

--- Week 3/HW2:2:17/sketch.js
+++ Week 3/HW2:2:17/sketch.js
@@ -1,13 +1,32 @@
+var pacmanx = 100;
+var pacmany = 250;
+var pacmanw = 75;
+var pacmanh = 70;
+
+var backgroundColor = 'navy'
+
+var randNUM = 0;
+
+var mapX = 0;
+
+
 function setup() {
 createCanvas(500,500);
+     background(204);
 
+    randNUM= random(254); //0-255
+    console.log(randNUM);
 }
 
 function draw() {
-    background(204);
+    
+    background(backgroundColor)
+    
+
+   
     //pacman
     fill(255, 204, 0);
-    arc(100,250,75,70,390,250);
+    arc(mouseX,mouseY,75,70,390,250);
     
     //yellow dots
     ellipse(150,250,10,10);
@@ -37,7 +56,7 @@ function draw() {
    
    
     //first ghost
-    fill(175, 100, 220);
+    fill(mouseX,mouseY,0)
     ellipse(250,250,58,70)
     triangle(250,280,260,280,255,297);
     triangle(240,280,250,280,245,297);
@@ -48,15 +67,18 @@ function draw() {
     ellipse(240,250,10,10);
     ellipse(260,250,10,10);
     
+    //map(mouseX,0,width,0,10)
+    
+    mapX = map(mouseX, 0, width, -2, 2);
     fill(175, 100, 220);
-    ellipse(240,250,2,2);
-    ellipse(260,250,2,2);
+    ellipse(240 + mapX,250,2,2);
+    ellipse(260 +mapX ,250,2,2);
     
     fill(255, 170, 238);
     line(240,260,260,260);
     
     //second ghost
-    fill(245, 14, 120);
+    fill(mouseX, mouseY, 0);
     ellipse(350,250,58,70)
     triangle(350,280,340,280,345,297);
     triangle(340,280,330,280,335,297);
@@ -68,18 +90,27 @@ function draw() {
     ellipse(360,250,10,10);
     
     fill(245, 14, 120);
-    ellipse(340,250,2,2);
-    ellipse(360,250,2,2);
+    ellipse(340+ mapX,250,2,2);
+    ellipse(360+ mapX,250,2,2);
     
     fill(255, 170, 238);
     line(340,260,360,260);
     
 
     
-    
-    
-         
-    
-    
 
+    
+    
+    
+}
+
+function mousePressed(){
+    //background(navy)
+    backgroundColor='turquoise'
+
+    
+}
+
+function mouseReleased(){
+    backgroundColor='navy'
 }
--- Week2 Notes/sketch.js
+++ Week2 Notes/sketch.js
@@ -9,6 +9,12 @@ var mouthX= 50;
 var mouthy= 300;
 var mouthW = 400;
 var mouthH = 60;
+var eye1X = 70;
+var eye1Y = 70;
+
+var eye2offset = 300;
+var pupilOffset = 20;
+
 
 function setup() {
 console.log(num);
@@ -25,7 +31,6 @@ console.log(message * trex);
     
     createCanvas (500,500);
     background(0,0,0);
-    background(255,0,0);
     
 console.log(width + "" + height);
 
@@ -44,21 +49,44 @@ console.log(message2);
 }
 
 function draw() {
+    fill(255);
 //  console.log(num);
 //console.log(message2);
 //    var message3 ="this only works in the draw";
 
     //x, y, width, height
     
+    //eye1
     ellipse(70,70,100,100);
+    fill(0,0,255);
+    ellipse(eye1X, eye1Y + pupilOffset, 50,50);
     
-    ellipse(width/2, height /2,50,50);
+    //nose
+    triangle(width/2, height/2,(width/2)+20,(height/2)+20,(width/2)-20,(height/2)+20);
     
+    //eye2
+    fill(255);
+    ellipse(eye1X + eye2offset, eye1Y, 100,100);
+    fill(0,0,255);
+    ellipse(eye1X + eye2offset, eye1Y + pupilOffset, 50,50);
+    
+    //mouth
     rect(mouthX, mouthy, mouthW, mouthH);
-    line(mouthX, mouthY + mouthH/2, mouthx + mouthW, mouthY + mouthH/2);
+    stroke(125);
+    strokeWeight(10);
+    line(mouthX, mouthy + mouthH/2, mouthX + mouthW, mouthy + mouthH/2);
+    noStroke();
+    
+    fill('red');
+    arc(width/2 +100 ,330,100,100,0, PI);
     
     
     
+
     
+}
+
+function mousePressed(){
+    background('navy');
     
 }
