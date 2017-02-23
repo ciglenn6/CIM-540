903761fcddf95f4a55579be8859e1442f3104f6d
ciglenn6 <ciglenn6@gmail.com>
2017-02-23 12:08:08 -0500

    Week 5 Homework

---
+++ Week 5/sketch.js
@@ -0,0 +1,14 @@
+var ellipseY = [0, 15, 30];
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background("violet");
+  noStroke();
+  ellipseMode(CENTER);
+  fill("pink");
+  for (var i = 0; i < ellipseY.length; i++) {
+   ellipse((i+1)*100, ellipseY[i], 50, 25);
+    ellipseY[i] += 1;
+  }
+}
