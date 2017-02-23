84020a96afbad79757cf4a70764b33b80cc80bc5
ciglenn6 <ciglenn6@gmail.com>
2017-02-23 12:29:44 -0500

    more rounded

--- Week 5/sketch.js
+++ Week 5/sketch.js
@@ -8,7 +8,7 @@ function draw() {
   ellipseMode(CENTER);
   fill("pink");
   for (var i = 0; i < ellipseY.length; i++) {
-   ellipse((i+1)*100, ellipseY[i], 50, 25);
+   ellipse((i+1)*100, ellipseY[i], 75, 75);
     ellipseY[i] += 1;
   }
 }
