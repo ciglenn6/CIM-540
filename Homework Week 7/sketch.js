9e0ff06d84c993024925bf0c005a20769da2b629
Chloe Glenn <ciglenn6@gmail.com>
2017-03-02 12:34:20 -0500

    Homework photo gallery

--- Homework Week 7/sketch.js
+++ Homework Week 7/sketch.js
@@ -1,7 +1,38 @@
+var colorful;
+var galaxy;
+var somany;
+var stacked;
+
+var donutArray = [];
+
+var donutIndex = 0;
+
 function setup() {
+    createCanvas(640,480);
+
+    
+    donutArray.push(loadImage("images/colorful.jpg"));
+    donutArray.push(loadImage("images/galaxy.jpg"));
+    donutArray.push(loadImage("images/so many.jpg"));
+    donutArray.push(loadImage("images/stacked.jpg"));
+
 
 }
 
 function draw() {
-  
+    
+    image(donutArray[donutIndex], 0, 0);
+    
+
+}
+
+function mousePressed() {
+    donutIndex = donutChooser();
+}
+
+function donutChooser(){
+    
+    var randomNum = int(random(0,3));
+    
+    return randomNum;
 }
