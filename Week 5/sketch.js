0412568b945f22d9a44ebbb3a875ce1f7bc29172
Chloe Glenn <ciglenn6@gmail.com>
2017-03-02 12:14:03 -0500

    updates

--- Week 5/sketch.js
+++ Week 5/sketch.js
@@ -1,4 +1,5 @@
 var ellipseY = [0, 15, 30];
+var ellipseX = [1,16,31]
 function setup() {
   createCanvas(400, 400);
 }
@@ -11,4 +12,8 @@ function draw() {
    ellipse((i+1)*100, ellipseY[i], 75, 75);
     ellipseY[i] += 1;
   }
+
+    for (var i = 0; i < ellipseX.length; i++) {
+   ellipse((i+1)*100, ellipseX[i], 20, 20);
+    ellipseX[i] += 1;
 }
